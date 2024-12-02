8c42d5027556b6ff9a5773757a778653ad8283d6
eliooooooo <eliott.burkle@gmail.com>
2024-12-02 10:41:55 +0100

    Build: Add tailwindcss support

--- App.js
+++ App.js
@@ -1,20 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+// import { StyleSheet, Text, View } from "react-native";
+import { Text, View } from "react-native";
+import "./assets/src/main.css";
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    // <View style={styles.container}>
+    <View className='flex-1 justify-center items-center bg-red-600'>
       <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style='auto' />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: "#fff",
+//     alignItems: "center",
+//     justifyContent: "center",
+//   },
+// });
