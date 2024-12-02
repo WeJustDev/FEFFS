adddc510d4e6b9460122dbab1ac5354d24e4b969
Tristan Gerber <tristan.gerber@etu.unistra.fr>
2024-12-02 17:38:08 +0100

    user login local storage

--- App.js
+++ App.js
@@ -1,24 +1,18 @@
 import { StatusBar } from "expo-status-bar";
-import { Text, View } from "react-native";
+import { createTamagui, TamaguiProvider } from "tamagui";
 
-// import tailwindCSS
-import "./assets/src/main.css";
+import "./assets/src/main.css"
 
-// import Tamagui component library
-import { createTamagui,TamaguiProvider } from 'tamagui'
-import defaultConfig from '@tamagui/config/v3'
-import { Button } from 'tamagui'
+import defaultConfig from "@tamagui/config/v3";
+import Navigation from "./assets/pages/Navigation"; // Import du gestionnaire de navigation
 
-const config = createTamagui(defaultConfig)
+const config = createTamagui(defaultConfig);
 
 export default function App() {
   return (
     <TamaguiProvider config={config}>
-      <View className='flex-1 justify-center items-center bg-red-600'>
-        <Text>Open up App.js to start working on your app!</Text>
-        <StatusBar style='auto' />
-        <Button>Lorem ipsum</Button>
-      </View>
+      <StatusBar style="auto" />
+      <Navigation />
     </TamaguiProvider>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./App.{js,jsx,ts,tsx}",
-    ".assets/components/*.{js,jsx,ts,tsx}",
-    ".assets/components/ui/*.{js,jsx,ts,tsx}",
+    "./assets/components/*.{js,jsx,ts,tsx}",
+    "./assets/components/ui/*.{js,jsx,ts,tsx}",
+    "./assets/pages/*.{js,jsx,ts,tsx}",
   ],
   presets: [require("nativewind/preset")],
   theme: {
