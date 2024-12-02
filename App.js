ea2bfcff84f2c97909a35c8c3e958a87c73bac96
eliooooooo <eliott.burkle@gmail.com>
2024-12-02 11:10:39 +0100

    Build: add Tamagui

--- App.js
+++ App.js
@@ -1,23 +1,24 @@
 import { StatusBar } from "expo-status-bar";
-// import { StyleSheet, Text, View } from "react-native";
 import { Text, View } from "react-native";
+
+// import tailwindCSS
 import "./assets/src/main.css";
+
+// import Tamagui component library
+import { createTamagui,TamaguiProvider } from 'tamagui'
+import defaultConfig from '@tamagui/config/v3'
+import { Button } from 'tamagui'
+
+const config = createTamagui(defaultConfig)
 
 export default function App() {
   return (
-    // <View style={styles.container}>
-    <View className='flex-1 justify-center items-center bg-red-600'>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style='auto' />
-    </View>
+    <TamaguiProvider config={config}>
+      <View className='flex-1 justify-center items-center bg-red-600'>
+        <Text>Open up App.js to start working on your app!</Text>
+        <StatusBar style='auto' />
+        <Button>Lorem ipsum</Button>
+      </View>
+    </TamaguiProvider>
   );
 }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: "#fff",
-//     alignItems: "center",
-//     justifyContent: "center",
-//   },
-// });
