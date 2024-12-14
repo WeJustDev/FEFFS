5ed455d79bbde9ca6bfc2201b50a6ccbd6d2168c
deveney <jeremy.deveney@etu.unistra.fr>
2024-12-14 16:05:42 +0100

    Fix couleur des inputs sur purchasemodal

--- components/PurchaseModal.tsx
+++ components/PurchaseModal.tsx
@@ -385,11 +385,6 @@ const styles = StyleSheet.create({
     paddingVertical: 12,
     paddingHorizontal: 15,
     fontSize: 16,
-    shadowColor: '#000',
-    shadowOffset: { width: 0, height: 1 },
-    shadowOpacity: 0.1,
-    shadowRadius: 2,
-    elevation: 2,
   },
   passDetailsContainer: {
     backgroundColor: 'rgba(255,255,255,0.05)',
